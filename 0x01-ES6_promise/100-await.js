4e883eca16e4248d8dc0824f7a6d7f1507b34c93
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-11-30 00:54:03 +0200

    await / async

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  let response_obj = {};
+  let res = {};
 
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    response_obj = { photo, user };
+    res = { photo, user };
   } catch (err) {
-    response_obj = { photo: null, user: null };
+    res = { photo: null, user: null };
   }
-  return response_obj;
+  return res;
 }
