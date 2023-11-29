1c4f948476fdc46b1be71af26103565fccc0183f
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-11-30 00:34:27 +0200

    await / async

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let response_obj = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    response_obj = { photo, user };
+  } catch (err) {
+    response_obj = { photo: null, user: null };
+  }
+  
+  return response_obj;
+}
