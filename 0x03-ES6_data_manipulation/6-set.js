0c98afcf804538f490857a293a2ccaf6988e0fd9
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-12-05 14:00:30 +0200

    set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Prof. Percival <https://github.com/Prof-Percival/>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
