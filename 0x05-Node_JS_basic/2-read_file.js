15a6ee4e35ae6959a62faeb42a711aa0fc03cacb
Prof-Percival <percival.mrd921@yahoo.com>
2024-02-01 19:40:57 +0200

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Prof. Percival <https://github.com/Prof-Percival>
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
